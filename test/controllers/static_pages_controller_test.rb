e7b4ec154e07c33bd0b97f80fbd01a44d8316fc2
Eric <eric.rob.olson@gmail.com>
2016-10-02 12:48:39 -0700

    Static-Pages: Added About page. Modified titles, and added tests for them.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,20 +1,22 @@
 require 'test_helper'
 
-#class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-#  test "should get home" do
-#    get static_pages_home_url
-#    assert_response :success
-#  end
-#
-#  test "should get help" do
-#    get static_pages_help_url
-#    assert_response :success
-#  end
-#end
-
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get about" do
     get :about
 	assert_response :success
+	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
+  
+  test "should get home" do
+    get :home
+	assert_response :success
+	assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+  end
+  
+  test "should get help" do
+    get :help
+	assert_response :success
+	assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  end
+  
 end
